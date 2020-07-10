ef001027310bd5af1cb62d24503d7eca04ad6c89
Leonardo Delmiro <leodelmiroms@gmail.com>
2020-07-10 12:58:46 -0300

    Getting user by Id

--- src/main/java/com/leodelmiro/springbootmongodb/services/UserService.java
+++ src/main/java/com/leodelmiro/springbootmongodb/services/UserService.java
@@ -2,10 +2,12 @@ package com.leodelmiro.springbootmongodb.services;
 
 import com.leodelmiro.springbootmongodb.domain.User;
 import com.leodelmiro.springbootmongodb.repositories.UserRepository;
+import com.leodelmiro.springbootmongodb.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,5 +17,10 @@ public class UserService {
 
     public List<User> findAll(){
         return userRepository.findAll();
+    }
+    
+    public User findById(String id){
+        Optional<User> obj = userRepository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 }
---
+++ src/main/java/com/leodelmiro/springbootmongodb/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,9 @@
+package com.leodelmiro.springbootmongodb.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException{
+    private static final long serialVersionUID = 1L;
+
+    public ObjectNotFoundException(String msg){
+        super(msg);
+    }
+}
