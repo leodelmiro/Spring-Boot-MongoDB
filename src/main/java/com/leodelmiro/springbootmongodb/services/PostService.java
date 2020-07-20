ee9526a7b6613501f93e13288d42521d2a81ecbd
Leonardo Delmiro <leodelmiroms@gmail.com>
2020-07-20 14:21:24 -0300

    Get PostById

---
+++ src/main/java/com/leodelmiro/springbootmongodb/services/PostService.java
@@ -0,0 +1,21 @@
+package com.leodelmiro.springbootmongodb.services;
+
+import com.leodelmiro.springbootmongodb.domain.Post;
+import com.leodelmiro.springbootmongodb.repositories.PostRepository;
+import com.leodelmiro.springbootmongodb.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository postRepository;
+    
+    public Post findById(String id){
+        Optional<Post> obj = postRepository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+}
