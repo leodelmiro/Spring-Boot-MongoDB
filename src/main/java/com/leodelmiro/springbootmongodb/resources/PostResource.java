d0fff1bd6a10e425c92770ac4d6bfe01cbee8eb1
Leonardo Delmiro <leodelmiroms@gmail.com>
2020-07-20 15:56:32 -0300

    Simple query using Query Methods

--- src/main/java/com/leodelmiro/springbootmongodb/resources/PostResource.java
+++ src/main/java/com/leodelmiro/springbootmongodb/resources/PostResource.java
@@ -1,13 +1,14 @@
 package com.leodelmiro.springbootmongodb.resources;
 
 import com.leodelmiro.springbootmongodb.domain.Post;
+import com.leodelmiro.springbootmongodb.resources.util.URL;
 import com.leodelmiro.springbootmongodb.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -20,5 +21,13 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id) {
         Post obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+
+    @GetMapping(value = "/titlesearch")
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 }
--- src/main/java/com/leodelmiro/springbootmongodb/services/PostService.java
+++ src/main/java/com/leodelmiro/springbootmongodb/services/PostService.java
@@ -6,6 +6,7 @@ import com.leodelmiro.springbootmongodb.services.exception.ObjectNotFoundExcepti
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -13,9 +14,13 @@ public class PostService {
 
     @Autowired
     private PostRepository postRepository;
-    
-    public Post findById(String id){
+
+    public Post findById(String id) {
         Optional<Post> obj = postRepository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public List<Post> findByTitle(String text) {
+        return postRepository.findByTitleContainingIgnoreCase(text);
     }
 }
