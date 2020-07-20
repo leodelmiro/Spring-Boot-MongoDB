eac0153d6e86ba2ec96001d189476bca4e40bf23
Leonardo Delmiro <leodelmiroms@gmail.com>
2020-07-20 13:57:40 -0300

    Projecting Author Data using DTO

--- src/main/java/com/leodelmiro/springbootmongodb/config/Instantiation.java
+++ src/main/java/com/leodelmiro/springbootmongodb/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.leodelmiro.springbootmongodb.config;
 
 import com.leodelmiro.springbootmongodb.domain.Post;
 import com.leodelmiro.springbootmongodb.domain.User;
+import com.leodelmiro.springbootmongodb.dto.AuthorDTO;
 import com.leodelmiro.springbootmongodb.repositories.PostRepository;
 import com.leodelmiro.springbootmongodb.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,11 +32,10 @@ public class Instantiation implements CommandLineRunner {
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
-
-        Post post1 = new Post(null, simpleDateFormat.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-        Post post2 = new Post(null, simpleDateFormat.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
-
         userRepository.saveAll(Arrays.asList(maria,alex,bob));
+
+        Post post1 = new Post(null, simpleDateFormat.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+        Post post2 = new Post(null, simpleDateFormat.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
         postRepository.saveAll(Arrays.asList(post1,post2));
     }
 }
